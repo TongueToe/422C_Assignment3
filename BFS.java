7c0ee3c1dbdb6ef5fc738004c01c8065ab8ac87a
mi4467 <irfan24@utexas.edu>
2017-02-20 16:52:38 -0600

    almost

--- BFS.java
+++ BFS.java
@@ -1,4 +1,4 @@
-package assignment3;
+package Assignment3v2;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.Iterator;
@@ -6,25 +6,32 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Queue;
 
 public class BFS {
-		public static ArrayList<String> getWordLadderBFS(String input, String output, Set<String> dict, ArrayList<String> ladder, LinkedList<String> visited){
-			ArrayList<String> adjacencyVertices = new ArrayList<String>();
-			while(ladder.contains(output)!=true){	//essentially gets neighbors of most recent rung, then decides to add a rung
-				adjacencyVertices = getAdjacentVerticies(ladder.get(ladder.size()-1), dict);	//get an array that has all the neighbors of a node, last rung essentially
-				addRung(ladder, adjacencyVertices, visited, dict, output);			//continues to add rungs until list finally gets word ladder
+		public static ArrayList<String> getWordLadderBFS(String input, String output, Set<String> dict, Queue<Word> queue, ArrayList<Word> tracker){
+			while(queue.isEmpty()!=true){														//Takes the head, checks to see if it's end of ladder, otherwise keeps on traversing
+				Word current = queue.poll();
+				current.beenVisited=true;
+				tracker.add(current);
+				if(current.word.equals(output)){
+					return constructLadder(tracker, input, output);
+				}
+				ArrayList<String> adjacencyVertices = getAdjacentVerticies(current.word, dict, tracker);	//gets adjacent string
+				addToQueue(adjacencyVertices, queue, current);												//adds strings as Word objects to the Q
 			}		
-			return ladder;		
+			return null;		
 		}
 		
-		public static ArrayList<String> getAdjacentVerticies(String input, Set<String> dict){		//takes vertex input, 
+		public static ArrayList<String> getAdjacentVerticies(String input, Set<String> dict, ArrayList<Word> tracker){		//takes vertex input, and collects adjacencies that have been  not been visited
 			Iterator dictionaryTraversal = dict.iterator();
 			ArrayList<String> neighbors = new ArrayList<String>();
 			while(dictionaryTraversal.hasNext()!=false){
 				String word =(String) dictionaryTraversal.next();
 				if(isNeighbor(input, word)==true){
-					neighbors.add(word);
-					//System.out.println(word);
+					if(hasItBeenVisited(word, tracker)!=true){			//checks to see if adjacency has been visited
+						neighbors.add(word);
+					}
 				}
 			}
 			return neighbors;
@@ -43,31 +50,39 @@ public class BFS {
 			return true;			
 		}
 		
-		public static int diffString(String vertex, String word){		//counts number of letters different in two strings
-			int difChars=0;
-			for(int i=0; i<vertex.length(); i++){
-				if(vertex.charAt(i)!=word.charAt(i)){
-					difChars++;
-				}
-			}
-			return difChars;			
+		
+		public static void addToQueue(ArrayList<String> adjacencyVertices, Queue<Word> queue, Word current){	
+			String s= "";
+			ListIterator<String> adjacencyTraversal = adjacencyVertices.listIterator();						//An iterator to travel through neighbor list
+			while(adjacencyTraversal.hasNext()!=false){														//turns string into word object, adds it to q
+				s = (String) adjacencyTraversal.next();
+				Word word = new Word(s, current);
+				queue.offer(word);
+			}	
+			return;																			
 		}
 		
-		public static void addRung(ArrayList<String> ladder, ArrayList<String> adjacencyVertices, LinkedList<String> visited, Set<String> dict, String output){
-			int difference = 999999999;
-			int difChars = 0;
-			int index =0;
-		
-			ListIterator<String> adjacencyTraversal = adjacencyVertices.listIterator();						//An iterator to travel through neighbor list
-			while(adjacencyTraversal.hasNext()!=false){										//finds neighbor that is closest to output
-				difChars = diffString((String) adjacencyTraversal.next(), output);
-				if(difChars<difference){
-					difference=difChars;
-					index = adjacencyTraversal.nextIndex()-1;								//keep index of rung closest to output
+		public static boolean hasItBeenVisited(String word, ArrayList<Word> tracker){
+			for(int i=0; i<tracker.size(); i++){
+				if(word.equals(tracker.get(i).word)){														//sees if vertex has been visited, if so, we don't put it in que
+					return true;
 				}
-			}	
-			ladder.add((String) adjacencyVertices.get(index));
-			visited.add((String) adjacencyVertices.get(index));
-			return;																			//pushes word closes to output to ladder, and marks the vertex as visited
+			}
+			return false;
+		}
+		
+		public static ArrayList<String> constructLadder(ArrayList<Word> tracker, String input, String output){
+			ArrayList<String> wordLadderTemp = new ArrayList<String>();
+			ArrayList<String> wordLadder = new ArrayList<String>();
+			Word current = tracker.get(tracker.size()-1);				//creates the word ladder
+			while(current!=null){
+				wordLadderTemp.add(current.word);
+				current =current.parent;			
+			}
+			for(int i=wordLadderTemp.size()-1; i>=0; i--){
+				wordLadder.add(wordLadderTemp.get(i));
+			}
+			return wordLadder;
+			
 		}
 }
---
+++ Word.java
@@ -0,0 +1,15 @@
+package Assignment3v2;
+
+public class Word {
+	String	word;
+	Word parent;							//this allows us to link back to the input
+	boolean beenVisited;				
+	
+	public Word(String word, Word parent){
+		this.word=word;
+		this.parent=parent;					//class meant to represent vertex with other properties
+		this.beenVisited=false;
+	}
+	
+	
+}
