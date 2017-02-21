e663010ce16751e71ed921bb9c7207166db017b1
mi4467 <irfan24@utexas.edu>
2017-02-21 07:57:08 -0600

    bit more smooth

--- BFS.java
+++ BFS.java
@@ -1,41 +1,35 @@
-package assignment3;
+package Assignment3v2;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.Iterator;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.ListIterator;
 import java.util.Queue;
 
 public class BFS {
 		public static ArrayList<String> getWordLadderBFS(String input, String output, Set<String> dict, Queue<Word> queue, ArrayList<Word> tracker){
 			while(queue.isEmpty()!=true){															//Takes the head, checks to see if it's end of ladder, otherwise keeps on traversing
 				Word current = queue.poll();
-				current.beenVisited=true;
 				tracker.add(current);
 				if(current.word.equals(output)){
 					return constructLadder(tracker, input, output);
 				}
-				ArrayList<String> adjacencyVertices = getAdjacentVerticies(current, dict, tracker);	//gets adjacent string
-				addToQueue(adjacencyVertices, queue, current);										//adds strings as Word objects to the Q
+				getAdjacentVerticies(current, dict, tracker, queue);	//gets adjacent string
 			}		
 			return null;		
 		}
 		
-		public static ArrayList<String> getAdjacentVerticies(Word current, Set<String> dict, ArrayList<Word> tracker){		//takes vertex input, and collects adjacencies that have been  not been visited
+		public static void getAdjacentVerticies(Word current, Set<String> dict, ArrayList<Word> tracker, Queue<Word> queue){		//takes vertex input, and collects adjacencies that have been  not been visited
 			Iterator dictionaryTraversal = dict.iterator();
-			ArrayList<String> neighbors = new ArrayList<String>();
 			while(dictionaryTraversal.hasNext()!=false){
 				String word =(String) dictionaryTraversal.next();
 				if(isNeighbor(current.word, word)==true){
 					if(hasItBeenVisited(word, tracker)!=true){			//checks to see if adjacency has been visited
-						neighbors.add(word);
-						tracker.add(new Word(word, current));			//marks it as visited
+						Word pizza = new Word(word, current);
+						queue.add(pizza);
+						tracker.add(pizza);			                    //marks it as visited
 					}
 				}
 			}
-			return neighbors;
+			return;
 		}
 		
 		public static boolean isNeighbor(String vertex, String word){		//takes a node, and some other word, and sees if they are connected
@@ -49,18 +43,6 @@ public class BFS {
 				return false;
 			}
 			return true;			
-		}
-		
-		
-		public static void addToQueue(ArrayList<String> adjacencyVertices, Queue<Word> queue, Word current){	
-			String s= "";
-			ListIterator<String> adjacencyTraversal = adjacencyVertices.listIterator();						//An iterator to travel through neighbor list
-			while(adjacencyTraversal.hasNext()!=false){														//turns string into word object, adds it to q
-				s = (String) adjacencyTraversal.next();
-				Word word = new Word(s, current);
-				queue.offer(word);
-			}	
-			return;																			
 		}
 		
 		public static boolean hasItBeenVisited(String word, ArrayList<Word> tracker){
