f9ca34a304744af43662781255284fdf69c0ace5
mi4467 <irfan24@utexas.edu>
2017-02-21 07:59:13 -0600

    changed word object

--- Word.java
+++ Word.java
@@ -1,15 +1,13 @@
-package assignment3;
+package Assignment3v2;
 
 public class Word {
 	String	word;
 	Word parent;							//this allows us to link back to the input
-	boolean beenVisited;				
+	//boolean beenVisited;				
 	
 	public Word(String word, Word parent){
 		this.word=word;
 		this.parent=parent;					//class meant to represent vertex with other properties
-		this.beenVisited=false;
+		//this.beenVisited=false;
 	}
-	
-	
 }
