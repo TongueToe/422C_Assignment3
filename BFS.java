150cc784d1f99d2baae6eb44fcb22336581fb982
mi4467 <irfan24@utexas.edu>
2017-02-21 22:14:40 -0600

    Update BFS.java

--- BFS.java
+++ BFS.java
@@ -6,13 +6,14 @@ import java.util.Queue;
 
 public class BFS {
 		/**
+		 * Constructs a BFS word ladder, and determines edge cases such as not in dictionary, or no word Ladder
 		 * 
-		 * @param input
-		 * @param output
-		 * @param dict
-		 * @param queue
-		 * @param tracker
-		 * @return
+		 * @param input, the start of the ladder
+		 * @param output, the bottom of the ladder
+		 * @param dict, the dictionary we use to find words to construct the ladder
+		 * @param queue, the mechanism for placing our words
+		 * @param tracker, to keep track of visited nodes, in order to avoid infinite search
+		 * @return Gives an ArrayList that is actually the Word Ladder
 		 */
 		public static ArrayList<String> getWordLadderBFS(String input, String output, Set<String> dict, Queue<Word> queue, ArrayList<Word> tracker){
 			if(input.equals(output)){
@@ -20,29 +21,27 @@ public class BFS {
 				ladder.add(input);
 				ladder.add(output);
 				return ladder;
-				}
-			
-			
-			while(queue.isEmpty()!=true){															//Takes the head, checks to see if it's end of ladder, otherwise keeps on traversing
+				}	
+			while(queue.isEmpty()!=true){														
 				Word current = queue.poll();
 				tracker.add(current);
 				if(current.word.equals(output)){
 					return constructLadder(tracker, input, output);
 				}
-				getAdjacentVerticies(current, dict, tracker, queue);	//gets adjacent string
+				getAdjacentVerticies(current, dict, tracker, queue);	
 			}
 			ArrayList<String> ladder = new ArrayList<String>();
 			ladder.add(input);
 			ladder.add(output);
 			return ladder;
-				//return null;
 		}
 		/**
+		 * Collects all unvisited adjacent nodes and puts them on the queue
 		 * 
-		 * @param current
-		 * @param dict
-		 * @param tracker
-		 * @param queue
+		 * @param current, a Word object that holds the word we are trying to find adjacent nodes for
+		 * @param dict, a dictionary that holds words
+		 * @param tracker, a list that keeps track of the visited words
+		 * @param queue, the mechanism for placing our words
 		 */
 		public static void getAdjacentVerticies(Word current, Set<String> dict, ArrayList<Word> tracker, Queue<Word> queue){		//takes vertex input, and collects adjacencies that have been  not been visited
 			Iterator dictionaryTraversal = dict.iterator();
@@ -59,10 +58,11 @@ public class BFS {
 			return;
 		}
 		/**
+		 * Method that decides if dictionary word is adjacent to current node
 		 * 
-		 * @param vertex
-		 * @param word
-		 * @return
+		 * @param vertex, the word we are trying to find a neighbor for
+		 * @param word, a word object for the dictionary word
+		 * @return a boolean to see if word is adjacent to node
 		 */
 		public static boolean isNeighbor(String vertex, String word){		//takes a node, and some other word, and sees if they are connected
 			int difChars=0;
@@ -78,10 +78,11 @@ public class BFS {
 		}
 		
 		/**
+		 * Method that sees if the node is visited
 		 * 
-		 * @param word
-		 * @param tracker
-		 * @return
+		 * @param word, the dictionary word we are trying to see if it has been visited
+		 * @param tracker, the list of visited nodes
+		 * @return a boolean that tells us if it has been visited
 		 */
 		public static boolean hasItBeenVisited(String word, ArrayList<Word> tracker){
 			for(int i=0; i<tracker.size(); i++){
@@ -93,11 +94,12 @@ public class BFS {
 		}
 		
 		/**
+		 * Tracks back and builds the word Ladder
 		 * 
-		 * @param tracker
-		 * @param input
-		 * @param output
-		 * @return
+		 * @param tracker, the list of visited node, in this case, used to get final word
+		 * @param input, the initial word in the ladder
+		 * @param output, the final word in the ladder
+		 * @return the Word Ladder
 		 */
 		public static ArrayList<String> constructLadder(ArrayList<Word> tracker, String input, String output){
 			ArrayList<String> wordLadderTemp = new ArrayList<String>();
